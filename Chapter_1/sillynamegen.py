0c48dc7ecfc811085dc876c1a9da34a2c1a9ef34
Azzam Slman <azzamss0.11@gmail.com>
2023-11-21 04:26:50 +0300

    adding Chapter_1 folder
    finishing sillynamegen.py

---
+++ Chapter_1/sillynamegen.py
@@ -0,0 +1,51 @@
+"""Generate funny names by randomly combining names from 2 separate lists."""
+import sys
+import random
+
+def main():
+    """Choose names at random from 2 tuples of names and print to screen."""
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+    "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite' ",
+    'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+    'Chewy', 'Chigger", "Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+    'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+    'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+    'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+    'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"',
+    'Mergatroid', '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch',
+    'Ovaltine', 'Pennywhistle', 'Pitchfork Ben', 'Potato Bug',
+    'Pushmeet','Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+    "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+    'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+    'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+    "Winston 'Jazz Hands'", 'Worms')
+
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+    'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+    'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+    'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+    'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+    'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+    'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+    'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+    'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+    'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+    'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+    'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+    'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+    'Woolysocks')
+
+    while True:
+        first_name = random.choice(first)
+        last_name = random.choice(last)
+        
+        print(f"\n{first_name} {last_name}\n", file = sys.stderr)
+
+        try_again = input("\n Try Agian? (Press Enter else q to quit)\n")
+        if try_again.lower() == "q":
+            break
+
+    input("Press Enter to exit.")
+
+if __name__  == "__main__":
+    main()
