cd863491ffcd0ca043ce047555b4d6a1b38cfbda
Azzam Slman <azzamss0.11@gmail.com>
2023-11-28 03:41:04 +0300

    2

--- Chapter_2/load_dictionary.py
+++ Chapter_2/load_dictionary.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sys
 def load(file):
     try:
@@ -11,18 +10,5 @@ def load(file):
         sys.exit(1)
     
 files = load("2of4brif.txt")
-=======
-import sys
-def load(file):
-    try:
-        with open(file) as in_file:
-            txt = in_file.read().strip().split('\n')
-            txt = [x.lower() for x in txt]
-            return txt
-    except IOError as e:
-        print(f"{e}Could not open the file {file}")
-        sys.exit(1)
-    
-files = load("2of4brif.txt")
->>>>>>> c083aa8ab0ba4b8d208fc48210652776074dfe5d
+
 print(files[1:33])
--- Chapter_2/palindromes.py
+++ Chapter_2/palindromes.py
@@ -1,1 +1,19 @@
-print("ok")
+def load(file):
+    try:
+        with open("2of4brif.txt") as the_file:
+            texts = the_file.read().strip().split("\n")
+            texts = [x.lower() for x in texts]
+            return texts
+    except IOError as n:
+        print(f"{n} :c ould not open the file")
+    
+    
+
+file = "2of4brif.txt"
+texts = load(file)
+palindromes = []   
+for text in texts:
+    if text == text[::-1]:
+        palindromes.append(text)
+
+print(palindromes)
